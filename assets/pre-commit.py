48588bd12aadb3ead047a6e4db0c011475988c5f
Fenhl <fenhl@fenhl.net>
2025-12-03 11:17:33 +0000

    Upgrade Nix dependencies

--- assets/pre-commit.py
+++ assets/pre-commit.py
@@ -11,23 +11,22 @@ for old_line in old_cargo_toml.splitlines():
         break
 else:
     sys.exit('Missing version number in old Cargo.toml')
-with open('Cargo.toml', encoding='utf-8') as f: #TODO check staged changes instead of worktree
-    for new_line in f:
-        if new_line.startswith('version = '):
-            break
-    else:
-        sys.exit('Missing version number in Cargo.toml')
+new_cargo_toml = subprocess.run(['git', 'show', ':Cargo.toml'], stdout=subprocess.PIPE, encoding='utf-8', check=True).stdout
+for new_line in new_cargo_toml.splitlines():
+    if new_line.startswith('version = '):
+        break
+else:
+    sys.exit('Missing version number in staged Cargo.toml')
 if old_line.strip() == new_line.strip():
     sys.exit('Missing version bump in Cargo.toml')
 
 old_cargo_lock = subprocess.run(['git', 'show', f'{against}:Cargo.lock'], stdout=subprocess.PIPE, encoding='utf-8', check=True).stdout
-with open('Cargo.lock', encoding='utf-8') as f: #TODO check staged changes instead of worktree
-    new_cargo_lock = f.read()
+new_cargo_lock = subprocess.run(['git', 'show', ':Cargo.lock'], stdout=subprocess.PIPE, encoding='utf-8', check=True).stdout
 if old_cargo_lock == new_cargo_lock: #TODO more precisely compare the version field
     sys.exit('Missing version bump in Cargo.lock')
 
 subprocess.run(['cargo', 'check'], check=True)
 if platform.system() == 'Windows':
-    subprocess.run(['wsl', 'rsync', '--delete', '-av', '/mnt/c/Users/fenhl/git/github.com/dasgefolge/sil/stage/', '/home/fenhl/wslgit/github.com/dasgefolge/sil/', '--exclude', 'target'], check=True) # copy the tree to the WSL file system to improve compile times
-    subprocess.run(['wsl', 'env', '-C', '/home/fenhl/wslgit/github.com/dasgefolge/sil', 'cargo', 'check'], check=True)
-    subprocess.run(['wsl', '-d', 'nixos-m2', 'nix', 'build', '--no-link'], check=True)
+    subprocess.run(['wsl', '-d', 'ubuntu-m2', 'rsync', '--mkpath', '--delete', '-av', '/mnt/c/Users/fenhl/git/github.com/dasgefolge/sil/stage/', '/home/fenhl/wslgit/github.com/dasgefolge/sil/', '--exclude', 'target'], check=True) # copy the tree to the WSL file system to improve compile times
+    subprocess.run(['wsl', '-d', 'ubuntu-m2', 'env', '-C', '/home/fenhl/wslgit/github.com/dasgefolge/sil', '/home/fenhl/.cargo/bin/cargo', 'check'], check=True)
+    subprocess.run(['wsl', 'nix', 'build', '--no-link'], check=True)
--- flake.nix
+++ flake.nix
@@ -1,16 +1,14 @@
 {
-    inputs = {
-        nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; # requires Rust 1.87 or higher (https://blog.rust-lang.org/2025/05/15/Rust-1.87.0/#precise-capturing-use-in-impl-trait-in-trait-definitions)
-    };
-    outputs = { self, nixpkgs-unstable }: let
+    inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+    outputs = { self, nixpkgs }: let
         supportedSystems = [
             "aarch64-darwin"
             "aarch64-linux"
             "x86_64-darwin"
             "x86_64-linux"
         ];
-        forEachSupportedSystem = f: nixpkgs-unstable.lib.genAttrs supportedSystems (system: f {
-            pkgs = import nixpkgs-unstable { inherit system; };
+        forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+            pkgs = import nixpkgs { inherit system; };
         });
     in {
         packages = forEachSupportedSystem ({ pkgs, ... }: let
