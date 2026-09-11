f807b098a5eb3bb33b01711b01433d4e2fac6a83
Fenhl <fenhl@fenhl.net>
2026-09-11 17:41:21 +0000

    Fix calendar event field name

--- src/state.rs
+++ src/state.rs
@@ -99,7 +99,7 @@ struct Event {
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct CalEvent {
-    pub(crate) programmpunkt_id: String,
+    pub(crate) programmpunkt: String,
     pub(crate) start: MaybeAwareDateTime,
     pub(crate) end: MaybeAwareDateTime,
     pub(crate) text: String,
@@ -175,7 +175,7 @@ impl Mode {
                 let Some(rtww_data) = &rtww_data else { return Ok(None) };
                 let now = Utc::now().with_timezone(timezone);
                 let Some(_) = calendar_events.iter().find(|cal_event|
-                    cal_event.programmpunkt_id == "rtww"
+                    cal_event.programmpunkt == "rtww"
                     && cal_event.start.to_maybe_local(Some(*timezone)).is_ok_and(|start| match start {
                         MaybeLocalDateTime::Nonlocal(_) => false,
                         MaybeLocalDateTime::Local(start) => start <= now,
